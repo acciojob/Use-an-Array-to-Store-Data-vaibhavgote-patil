4a0d74e51585fe5208760e5059d5bcbd1adf0235
vaibhavgote-patil <120882845+vaibhavgote-patil@users.noreply.github.com>
2023-08-18 15:09:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 
+let complexArray = [
+  [
+    {
+      one: 1,
+      two: 2
+    },
+    {
+      three: 3,
+      four: 4
+    }
+  ],
+  [
+    {
+      a: "a",
+      b: "b"
+    },
+    {
+      c: "c",
+      d: "d"
+    }
+  ]
+];
